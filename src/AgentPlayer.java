edbdb978ef265847d0f78a468e278f3128e2487a
ledicia pier <lediciapier@gmail.com>
2018-12-27 20:05:55 +0100

    refactor minimax algorithm

--- src/AgentPlayer.java
+++ src/AgentPlayer.java
@@ -4,8 +4,9 @@ public class AgentPlayer extends Player {
 
 	@Override
 	public void makePlay(Scanner scanner, Table table, int T) {
-		Edge edge = Minimax.getNextPlay(table, 2);
-		System.out.println(edge);
+		Tree tree = Minimax.constructTree(table);
+		Edge edge = tree.checkWin();
+		System.out.println("minimax play: " + edge);
 		table.insertPlayAsAgent(edge.getX(), edge.getY(), edge.isHorizontal(), T);;
 	}
 
---
+++ src/Tree.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class Tree {
+    private Node root;
+
+    Tree() {
+    }
+
+    Node getRoot() {
+        return root;
+    }
+
+    void setRoot(Node root) {
+        this.root = root;
+    }
+
+	public Edge checkWin() { // TODO: change name
+		Node root = this.getRoot();
+		checkWin(root);
+		Node bestPlay = root.findBestChild();
+		return bestPlay.getEdge();
+	}
+ 
+	public void checkWin(Node node) { // TODO: change name
+		List<Node> children = node.getChildren();
+		boolean isMaxPlayer = node.isMaxPlayer(); // COMPROBAR SI VA A ASI O NEGAO
+		children.forEach(child -> {
+			if (child.getChildren().isEmpty()) {
+				child.calcUtility(); // TODO COGER MAX PLAYER COMO PARÁMETRO!!!
+			} else {
+				checkWin(child);
+			}
+		});
+		Node bestChild = node.findBestChild();
+		node.setUtility(bestChild.getUtility());
+    }
+    
+
+	
+
+}
