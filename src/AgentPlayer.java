0435485a516b8b3f86e26ca78f3807f0624179d9
ledicia pier <lediciapier@gmail.com>
2019-01-10 23:44:26 +0100

    use levels for tree construction instead of timeout

--- src/AgentPlayer.java
+++ src/AgentPlayer.java
@@ -6,7 +6,7 @@ public class AgentPlayer extends Player {
 	public void makePlay(Scanner scanner, Table table, int T) {
 		Tree tree = Minimax.constructTree(table);
 		Edge edge = tree.checkBest();
-		table.insertPlayAsAgent(edge.getX(), edge.getY(), edge.isHorizontal(), T);;
+		table.insertPlayAsAgent(edge.getX(), edge.getY(), edge.isHorizontal(), T);
 	}
 
 }
